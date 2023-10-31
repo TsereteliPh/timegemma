e6275db652efa59ad72ecae2bde0dc232143b830
Tsereteli.Ph <filipptsereteli@yandex.ru>
2023-10-31 21:11:41 +0300

    add and done 404 page

--- 404.php
+++ 404.php
@@ -2,11 +2,18 @@
 
 <section class="not-found">
 	<div class="container not-found__container">
-		<div class="not-found__text">
-			<h1 class="not-found__title">СТРАНИЦА НЕ НАЙДЕНА</h1>
-			<p>Такой страницы не существует.</p>
-			<p>Воспользуйтесь поиском по сайту.</p>
+		<div class="not-found__img">
+			<span>Fehler</span>
 		</div>
+
+		<h1 class="not-found__title">Seite nicht gefunden</h1>
+
+		<a href="<?php echo bloginfo( 'url' ); ?>" class="btn btn--dark btn--dial not-found__link">
+			Zur Hauptsache
+			<div class="btn__dial">
+				<svg width="83" height="76"><use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/sprite.svg#icon-arrow-dial"></use></svg>
+			</div>
+		</a>
 	</div>
 </section>
 
